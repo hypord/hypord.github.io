6b77b7ed9598fd706f22f4b7a6d01e2b5d0a160f
MCorange99 <gvidasjuknevicius2@gmail.com>
2022-06-19 19:06:39 +0300

    asd

--- login/js/emailLogin.js
+++ login/js/emailLogin.js
@@ -1,5 +1,7 @@
 
-function tryEmailRegister(){
+function tryEmailLogin(){
+    var baseUrl = "http://localhost:3000"
+
     var email = document.getElementById("login-inp-email").value;
     var password = document.getElementById("login-inp-password").value;
 
@@ -13,15 +15,19 @@ function tryEmailRegister(){
     }
 
     var req = new XMLHttpRequest();
-    req.open("POST", "http://base.mcorange.tk:3000/api/users/login");
+    req.open("GET", baseUrl + "/api/v1/user/getUser");
         req.send(data);
-        req.onload = () => {
+
+        req.onreadystatechange = () => {
+            if(req.readyState == 4) {
+            var resp = JSON.parse(req.responseText)
             if (req.status === 500) {
-                alert(`There was an error with that. Please try again. Error Code: ${req.status} \n If the issue persists, please get support at https://discord.gg/845cUp3YHk`);
-            } else if (req.status === 200) {
-                //window.location.replace(`http://hypord.mcorange.tk/chat&token=${}`);
-            } else {
-                alert(`An error occured. Please try logging in again. Error Code: ${req.status} \n If the issue persists, please get support at https://discord.gg/845cUp3YHk`);
+                    alert(`There was an error with that. Please try again. Error Code: ${req.status} \n If the issue persists, please get support at https://discord.gg/845cUp3YHk \n\n${resp.code}\n${resp.message}`);
+                } else if (req.status === 200) {
+                    //window.location.replace(`http://hypord.mcorange.tk/chat&token=${}`);
+                } else {
+                    alert(`An error occured. Please try logging in again. Error Code: ${req.status} \n If the issue persists, please get support at https://discord.gg/845cUp3YHk \n\n${resp.code}\n${resp.message}`);
+                }
             }
         }
 }
--- register/js/emailRegister.js
+++ register/js/emailRegister.js
@@ -1,5 +1,8 @@
+var config = require("../../config.json")
 
 function tryEmailRegister(){
+    var baseUrl = "http://localhost:3000"
+
     var email = document.getElementById("reg-inp-email").value;
     var username = document.getElementById("reg-inp-username").value;
     var displayName = document.getElementById("reg-inp-displayname").value;
@@ -17,17 +20,20 @@ function tryEmailRegister(){
         "displayName": displayName,
         "password": password
     }
-
+    data = JSON.stringify(data)
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     var req = new XMLHttpRequest();
-    req.open("POST", "http://base.mcorange.tk:3000/api/users/register");
+    req.open("POST", baseUrl + "/api/v1/user/postUser");
+                console.lo
                 req.send(data);
                 req.onload = () => {
                     if (req.status === 500) {
-                        alert(`There was an error with that. Please try again. Error Code: ${req.status} \n If the issue persists, please get support at https://discord.gg/845cUp3YHk`);
+                        alert(`There was an error with that. Please try again. Error Code: ${req.status} \n If the issue persists, please get support at https://discord.gg/845cUp3YHk\n ${req.code}`);
                     } else if (req.status === 200) {
                         //window.location.replace(`http://hypord.mcorange.tk/chat&token=${}`);
+                        alert("Done!")
                     } else {
-                        alert(`An error occured. Please try logging in again. Error Code: ${req.status} \n If the issue persists, please get support at https://discord.gg/845cUp3YHk`);
+                        alert(`An error occured. Please try logging in again. Error Code: ${req.status} \n If the issue persists, please get support at https://discord.gg/845cUp3YHk\n`);
                     }
     }
 }
